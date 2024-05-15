dab931bc7876bd2e03dce20a77002a8bca502745
Kversum <mayki1@mail.ru>
2024-05-15 22:40:16 +0300

    Add files via upload

---
+++ mod2_homework_for.py
@@ -0,0 +1,14 @@
+# cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+# for i in cars:
+#     print('Я езжу на автомабиле марки:', i)
+
+
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = int(0)
+
+for i in cars:
+    i == cars_count
+    print('Я езжу на автомабиле марки:', i, cars_count)
+    cars_count += 10
+
+
