a506ffc63235b29a765f37cdadfe8b906a85ec9e
Kversum <mayki1@mail.ru>
2024-06-03 00:10:02 +0300

    Add files via upload

--- mod3_homework_Recursiv_of_digits.py
+++ mod3_homework_Recursiv_of_digits.py
@@ -1,12 +1,15 @@
 def get_multiplied_digits(number):
     str_number = str(number)
     first = str_number[0]
-    for i in str_number[1:]:
-        if int(i) > 0:
-            temp = int(first) * (int(i))
-            first = temp
+    if len(str_number) > 1:
+        for i in str_number[1:]:
+            if int(i) > 0:
+                temp = int(first) * (int(i))
+                first = temp
+    else:
+        result = first
+        return  result
     result = first
     return result
-
 
 print(get_multiplied_digits(40203))
