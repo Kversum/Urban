3c77946570b6546a7e8a23c76364dee641ccb7d6
Kversum <mayki1@mail.ru>
2024-11-19 22:39:08 +0300

    Add files via upload
    Практическое задание по уроку "Строки и индексация строк"

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+# Практическое задание по уроку "Строки и индексация строк"
+
+example = 'Привет'
+print(example[0])
+print(example[-1])
+print(example[3:])
+print(example[::-1])
+print(example[1::2])
