4de78ad5d0e923641f7eade5d27052af8c395d04
Kversum <mayki1@mail.ru>
2024-11-24 23:58:09 +0300

    Домашняя работа по уроку "Пространство имён"
    Задача "Счётчик вызовов":
    Порой необходимо отслеживать, сколько раз вызывалась та или иная функция. К сожалению, в Python не предусмотрен подсчёт вызовов автоматически.
    Давайте реализуем данную фишку самостоятельно!
    
    
    
    Вам необходимо написать 3 функции:
    Функция count_calls подсчитывающая вызовы остальных функций.
    Функция string_info принимает аргумент - строку и возвращает кортеж из: длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
    Функция is_contains принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке, False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
    Пункты задачи:
    Создать переменную calls = 0 вне функций.
    Создать функцию count_calls и изменять в ней значение переменной calls. Эта функция должна вызываться в остальных двух функциях.
    Создать функцию string_info с параметром string и реализовать логику работы по описанию.
    Создать функцию is_contains с двумя параметрами string и list_to_search, реализовать логику работы по описанию.
    Вызвать соответствующие функции string_info и is_contains произвольное кол-во раз с произвольными данными.
    Вывести значение переменной calls на экран(в консоль).
    
    
    Пример результата выполнения программы:
    Пример выполняемого кода:
    print(string_info('Capybara'))
    print(string_info('Armageddon'))
    print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
    print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
    print(calls)
    
    Вывод на консоль:
    (8, 'CAPYBARA', 'capybara')
    (10, 'ARMAGEDDON', 'armageddon')
    True
    False
    4
    
    Примечания:
    Для использования глобальной переменной внутри функции используйте оператор global.
    Для функции is_contains лучше привести и искомую строку и все строки в списке в один регистр.
    Файл module_3_1.py и загрузите его на ваш GitHub репозиторий. В решении пришлите ссылку на него.
    
    Успехов!

---
+++ module_3_1.py
@@ -0,0 +1,28 @@
+calls = 0
+def count_calls():  # Подсчитывает вызовы остальных функций
+    global calls
+    calls += 1
+def string_info(stroka):
+    count_calls()
+    # принимает аргумент - строку и возвращает кортеж из длины этой строки, строку в верхнем регистре,       строку в нижнем регистре.
+    kortej = (len(stroka), stroka.upper(), stroka.lower())
+    return kortej
+
+def is_contains(string, list_to_search):
+    count_calls()
+    # Принимает два аргумента: строку и список. Возвращает True, если строка находится в списке, иначе False.
+    string_l = string.lower()
+    for n in list_to_search:
+        if string_l == n.lower():
+            match = True
+            break
+        else:
+            match = False
+    return (match)
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print('Кол-во вызовов функций:', calls)
